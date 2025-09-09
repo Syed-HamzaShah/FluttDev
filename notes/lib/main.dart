132fb393298bfeb42d416105f7a8662bd1ba320e
Syed Hamza Shah <hsworkshop7@gmail.com>
2025-09-09 19:06:17 +0500

    annon key removal

--- notes/lib/main.dart
+++ notes/lib/main.dart
@@ -4,8 +4,7 @@ import 'package:notes/note_database.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 const supabaseUrl = 'https://mxxlrugccwfctcdytoxe.supabase.co';
-const supabaseKey =
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im14eGxydWdjY3dmY3RjZHl0b3hlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTczNDIxNDYsImV4cCI6MjA3MjkxODE0Nn0.BzA_l7wkRVvnjukkJdBMY19mNOcmhcysDB50qHKvCPs';
+const supabaseKey = String.fromEnvironment('SUPABASE_KEY');
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
